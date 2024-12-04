3165d56d78043fb4b7e2046a97211a4ad7cc7450
Mazharaliruk <97372288+Mazharaliruk@users.noreply.github.com>
2024-12-04 11:39:11 +0500

    urls updated

--- lib/core/constants/api_urls.dart
+++ lib/core/constants/api_urls.dart
@@ -10,30 +10,30 @@ class ApiUrls{
   // Inventry endpoints
   static const String inventry = "${baseUrl}inventry/v1/";
   static const String categories = "${inventry}categories/";
-  static const String subCategories = "$baseUrl${inventry}subcategories/";
-  static const String promotion = "$baseUrl${inventry}promotions/";
-  static const String dicount = "$baseUrl${inventry}discounts/";
-  static const String taxe = "$baseUrl${inventry}taxes/";
+  static const String subCategories = "${inventry}subcategories/";
+  static const String promotion = "${inventry}promotions/";
+  static const String dicount = "${inventry}discounts/";
+  static const String taxe = "${inventry}taxes/";
 
   // sales endpoints
   static const String sales = "${baseUrl}sales/v1/";
-  static const String salesOrder = "$baseUrl${sales}orders/";
-  static const String transaction = "$baseUrl${sales}transactions/";
-  static const String payment = "$baseUrl${sales}payments/";
-  static const String paymentGetway = "$baseUrl${sales}paymentgetways/";
+  static const String salesOrder = "${sales}orders/";
+  static const String transaction = "${sales}transactions/";
+  static const String payment = "${sales}payments/";
+  static const String paymentGetway = "${sales}paymentgetways/";
 
   //core endpoints
   static const String core = "${baseUrl}core/v1/";
-  static const String businessType = "$baseUrl${core}businesstypes/";
-  static const String business = "$baseUrl${core}businesses/";
-  static const String event = "$baseUrl${core}events/";
-  static const String services = "$baseUrl${core}services/";
-  static const String eventServices = "$baseUrl${core}eventservices/";
-  static const String eventmedia = "$baseUrl${core}eventmedia/";
-  static const String reviews = "$baseUrl${core}reviews/";
-  static const String notifications = "$baseUrl${core}notifications/";
-  static const String aiRecommendation = "$baseUrl${core}AIRecommendation/";
-  static const String chatroom = "$baseUrl${core}chatrooms/";
-  static const String messages = "$baseUrl${core}messages/";
+  static const String businessType = "${core}businesstypes/";
+  static const String business = "${core}businesses/";
+  static const String event = "${core}events/";
+  static const String services = "${core}services/";
+  static const String eventServices = "${core}eventservices/";
+  static const String eventmedia = "${core}eventmedia/";
+  static const String reviews = "${core}reviews/";
+  static const String notifications = "${core}notifications/";
+  static const String aiRecommendation = "${core}AIRecommendation/";
+  static const String chatroom = "${core}chatrooms/";
+  static const String messages = "${core}messages/";
 
 }
